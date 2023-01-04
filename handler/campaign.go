8c4ea28210ccabbb9ebec0549b556d0c5a58cf54
hardzal <suryadijogja@gmail.com>
2023-01-04 23:36:46 +0700

    add handler

--- handler/campaign.go
+++ handler/campaign.go
@@ -29,13 +29,13 @@ func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 	campaigns, err := h.service.GetCampaigns(userID)
 
 	if err != nil {
-		response := helper.APIResponse("Error to get campaigns", http.StatusBadRequest, "error", nil)
+		response := helper.APIResponse("Error to get campaigns", http.StatusBadRequest, "error", campaign.FormatCampaigns(campaigns))
 
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
 
-	response := helper.APIResponse("List of all campaigns", http.StatusOK, "success", campaigns)
+	response := helper.APIResponse("List of all campaigns", http.StatusOK, "success", campaign.FormatCampaigns(campaigns))
 	c.JSON(http.StatusOK, response)
 
 }
